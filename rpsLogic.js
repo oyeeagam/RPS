5864e0e046d3e96001aa5a3cf0db50a8bd541550
Agam Sharma <168130074+oyeeagam@users.noreply.github.com>
2026-01-03 00:56:22 +0000

    fix: return type of functions

--- rpsLogic.js
+++ rpsLogic.js
@@ -1,6 +1,8 @@
 function getComputerChoice(){
     let val =  Math.floor(Math.random()*3)
-    return val
+    if(val==0) return "rock"
+    if(val==1) return "paper"
+    if(val==2) return "scissors"
 }
 
 function getUserChoice(){
@@ -8,8 +10,13 @@ function getUserChoice(){
     console.log("1. Rock")
     console.log("2. Paper")
     console.log("3. Scissors")
-    let choice = parseInt(prompt());
+    let choice = (prompt());
     return choice
 }
 
-console.log(getUserChoice())
+let userScore=0
+let computerScore=0
+
+function playRound(userChoice,computerChoice){
+
+}
