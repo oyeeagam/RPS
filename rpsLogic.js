989e363e39164a1bcae634e8d9441dd561193665
Agam Sharma <168130074+oyeeagam@users.noreply.github.com>
2026-01-03 00:52:29 +0000

    feat:added user choice

--- rpsLogic.js
+++ rpsLogic.js
@@ -2,4 +2,14 @@ function getComputerChoice(){
     let val =  Math.floor(Math.random()*3)
     return val
 }
-console.log(getComputerChoice());
+
+function getUserChoice(){
+    console.log("choose your hand:")
+    console.log("1. Rock")
+    console.log("2. Paper")
+    console.log("3. Scissors")
+    let choice = parseInt(prompt());
+    return choice
+}
+
+console.log(getUserChoice())
