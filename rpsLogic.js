34b6e5455a21fa9c2d6e55fd8fd88ab21bdeac3d
Agam Sharma <168130074+oyeeagam@users.noreply.github.com>
2026-01-03 01:04:28 +0000

    feat: added play round function

--- rpsLogic.js
+++ rpsLogic.js
@@ -18,5 +18,34 @@ let userScore=0
 let computerScore=0
 
 function playRound(userChoice,computerChoice){
+    userChoice=userChoice.toLowerCase()
 
+    if(userChoice.isEquals(computerChoice)){
+        console.log(`its a tie : both chose ${userChoice}`)
+    }
+
+    if(userChoice=='rock'&&computerChoice=='scissors'){
+        console.log(`you won: rock beats scissors`)
+        userScore++
+    }
+    else if(userChoice=='rock'&&computerChoice=='paper'){
+        console.log(`you lost: paper beats rock`)
+        computerScore++
+    }
+    else if(userChoice=='paper'&&computerChoice=='scissors'){
+        console.log(`you lost: scissors beats paper`)
+        computerScore++
+    }
+    else if(userChoice=='paper'&&computerChoice=='rock'){
+        console.log(`you won: paper beats rock`)
+        userScore++
+    }
+    else if(userChoice=='scissors'&&computerChoice=='paper'){
+        console.log(`you won: scissors beats paper`)
+        userScore++
+    }
+    else if(userChoice=='scissors'&&computerChoice=='rock'){
+        console.log(`you lost: rock beats scissors`)
+        computerScore++
+    }
 }
